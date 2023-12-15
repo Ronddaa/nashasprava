76c0641704ca30394fc69cb86eeaa607b4cb4cc8
Ronddaa <tronddaa@gmail.com>
2023-12-15 13:47:27 +0100

    fix bag swiper

--- src/main.js
+++ src/main.js
@@ -18,4 +18,39 @@ const swiperAuction = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
     draggable: true
   },
+
+  grabCursor: true,
+
+  mousewheel: {
+    sensitivity: 1,
+    eventTarget: '.swiper-slider'
+  },
+
+  autoHeight: true,
+
+  spaceBetween: 20,
+
 });
+
+function checkWidthWindow() {
+  // Получаем ширину экрана
+  const screenWidth = window.innerWidth;
+
+  // Условие для проверки ширины экрана
+  if (screenWidth <= 1157) {
+    // Изменяем параметры Swiper для 1 слайда
+    swiperAuction.params.slidesPerView = 1;
+  } else {
+    // Изменяем параметры Swiper для 2 слайдов
+    swiperAuction.params.slidesPerView = 2;
+  }
+
+  // Обновляем Swiper после изменения параметров
+  swiperAuction.update();
+}
+
+// Вызываем функцию при загрузке страницы
+checkWidthWindow();
+
+// Слушаем событие изменения размеров окна
+window.addEventListener('resize', checkWidthWindow);
